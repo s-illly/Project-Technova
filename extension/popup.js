9e9d5872fe2f6d047794eddcc3dcef21c4048fe1
daynaaaa <danya.eggy@gmail.com>
2024-09-28 11:03:05 -0400

    delete some stuff

--- extension/popup.js
+++ extension/popup.js
@@ -13,17 +13,6 @@ document.getElementById('b1').addEventListener('click', function () {
     img.src = imageName;
 });
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    chrome.tabs.executeScript(details.tabId, {
-        code: ' if (document.body.innerText.indexOf("Cat") !=-1) {' +
-              '     alert("Cat not found!");' +
-              ' }'
-    });
-}, {
-    url: [{
-        hostContains: '.com'
-    }],
-});
 
 function updateClock() {
     const { remainingTime } = timer;
