8838579b37f47d6a7947c34b17ffe042b9331242
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-09-28 14:12:38 -0400

    popup buttons

--- extension/popup.js
+++ extension/popup.js
@@ -1,16 +1,23 @@
 
-var images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png", "images/9.png", "images/10.png", "images/11.png"];
+const images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png", "images/9.png", "images/10.png", "images/11.png"];
 
-document.getElementById('b1').addEventListener('click', function () {
-    const img = document.getElementById('image');
-    const randomIdx = Math.floor(Math.random() * images.length);
-    const imageName = images[randomIdx];
-    img.src = imageName;
-});
+//document.getElementById('b1').addEventListener('click', function () {
+    //const img = document.getElementById('image');
+    //const randomIdx = Math.floor(Math.random() * images.length);
+    //const imageName = images[randomIdx];
+    //img.src = imageName;
+//});
 
 document.getElementById('start-session').addEventListener('click', () => {
     startCountdown(20 * 60); //20 min countdown
-    chrome.runtime.sendMessage({ action: 'startTimer' });
+    chrome.runtime.sendMessage({ action: 'startTimer' }); //send message to background to start timer
+
+    //select img element to be displayed
+    const img = document.getElementById('image');
+    
+    const randomIdx = Math.floor(Math.random() * images.length);
+    const imageName = images[randomIdx];
+    img.src = imageName;
 });
 
 function startCountdown(duration) {
