32d858839c210c8f85bb7fc6c8088a4303527a3a
daynaaaa <danya.eggy@gmail.com>
2024-09-28 10:59:08 -0400

    update popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,37 @@
-console.log('This is a popup!');
+//window.onload = chooseImage
+//content.document.defaultView.wrappedJSObject.chooseImage();
+//document.getElementById("b1").addEventListener('click', chooseImage);
+var images = ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png", "images/9.png", "images/10.png", "images/11.png"];
+
+document.getElementById('b1').addEventListener('click', function () {
+    const img = document.getElementById('image');
+    // Select a random image from the list
+    const randomIdx = Math.floor(Math.random() * images.length);
+    const imageName = images[randomIdx];
+
+    // Set the selected image as the source of the <img> element
+    img.src = imageName;
+});
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    chrome.tabs.executeScript(details.tabId, {
+        code: ' if (document.body.innerText.indexOf("Cat") !=-1) {' +
+              '     alert("Cat not found!");' +
+              ' }'
+    });
+}, {
+    url: [{
+        hostContains: '.com'
+    }],
+});
+
+function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+  
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+  }
